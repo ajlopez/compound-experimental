b689d731a660bfdf8f3c1d4b15485f627f1cbe74
ajlopez <webmaster@ajlopez.com>
2020-02-18 15:22:16 -0300

    CToken deploy arguments

--- commands/deploy.cmd
+++ commands/deploy.cmd
@@ -8,6 +8,6 @@ node deploy root tokendai FaucetToken 1000000,Dai,0,DAI
 node deploy root interestratemodel WhitePaperInterestRateModel 10000000000000000,10000000000000000
 
 node deploy root ctokenzrx CErc20
-node invoke root ctokenzrx initialize(address,address,address,uint256,string,string,uint8) tokenzrx,controller,interestratemodel,10000000000000000,0_x,ZRX,0
+node invoke root ctokenzrx initialize(address,address,address,uint256,string,string,uint8) "tokenzrx,controller,interestratemodel,10000000000000000,Test 0_x,cZRX,0"
 node deploy root ctokendai CErc20
-node invoke root ctokendai initialize(address,address,address,uint256,string,string,uint8) tokendai,controller,interestratemodel,10000000000000000,Dai,DAI,0
+node invoke root ctokendai initialize(address,address,address,uint256,string,string,uint8) "tokendai,controller,interestratemodel,10000000000000000,Test Dai,cDAI,0"
